a4eadef09d33894498203838e6fa11fb1f107a99
E1min <elmin708@gmail.com>
2024-03-07 20:34:14 +0400

    FreeWorkGrid

---
+++ GoogleMeetGrid/main.js
@@ -0,0 +1,19 @@
+let container = document.querySelector("div.container")
+let allcontainer = document.querySelectorAll("div.container")
+let gridbox = document.querySelector("div.gridbox")
+let allgridbox = document.querySelectorAll("div.gridbox")
+let btn = document.querySelector("button")
+
+function add() {
+    btn.addEventListener("click",todo = () => {
+        setTimeout(todo, 2000);
+        container.innerHTML += `<div class="gridbox">
+        <img src="./image/istockphoto-1196172395-612x612.jpg" alt="">
+        <h6>Ad Soyad</h6>
+    </div>`
+
+    })
+}
+
+add()
+
