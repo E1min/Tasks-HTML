e69bc0045bb187056c48b09ad536b7dd471c4612
E1min <elmin708@gmail.com>
2024-03-21 01:45:46 +0400

    FreeWork TipCalculate

---
+++ TipCalculate/main.js
@@ -0,0 +1,57 @@
+let billinput = document.querySelector(".bill input");
+let numpeopleinput = document.querySelector(".numpeople input");
+let tips = document.querySelectorAll(".tips");
+let custom = document.querySelector(".tips-in input");
+let displayAmount = document.querySelector(".display-amount")
+let displayTotal = document.querySelector(".display-total")
+let notfelem = document.querySelector(".notf");
+let resetbtn = document.querySelector(".button button");
+let percent = [];
+
+tips.forEach(item => {
+    item.addEventListener("click", () => {
+        tips.forEach(item => item.classList.remove("active"))
+        item.classList.toggle("active")
+        percent.unshift(item.textContent.slice(0, -1));
+        custom.value = ""
+        displayAmount.textContent = `$${((billinput.value * percent[0]) / 100) / numpeopleinput.value}`
+        displayTotal.textContent = `$${(+billinput.value + (billinput.value * percent[0]) / 100) / numpeopleinput.value}`
+        if (displayTotal.textContent !== "$0.00") {
+            resetbtn.className = "btnactive"
+        }
+    })
+
+
+    resetbtn.addEventListener("click", () => {
+        displayTotal.textContent = "$0.00"
+        displayAmount.textContent = "$0.00"
+        numpeopleinput.value = 1;
+        resetbtn.className = ""
+        billinput.value = ""
+        custom.value = ""
+        item.classList.remove("active")
+
+    })
+    custom.addEventListener("keyup", () => {
+        item.classList.remove("active")
+        displayAmount.textContent = `$${((billinput.value * custom.value) / 100) / numpeopleinput.value}`
+        displayTotal.textContent = `$${(+billinput.value + (billinput.value * custom.value) / 100) / numpeopleinput.value}`
+
+    })
+
+})
+
+
+numpeopleinput.addEventListener("keyup", () => {
+    if (numpeopleinput.value == "" || numpeopleinput.value < 1) {
+        notfelem.innerHTML = "Can't be zero"
+        numpeopleinput.classList.add("numred")
+    } else {
+        numpeopleinput.classList.remove("numred")
+        notfelem.innerHTML = ""
+        displayAmount.textContent = `$${((billinput.value * percent[0]) / 100) / numpeopleinput.value}`
+        displayTotal.textContent = `$${(+billinput.value + (billinput.value * percent[0]) / 100) / numpeopleinput.value}`
+    }
+
+
+})
