5db01a59b3d409774b6cdf9453ad32b59779b0f1
E1min <elmin708@gmail.com>
2024-03-22 15:25:58 +0400

    TipCalcUpdate

--- TipCalculate/main.js
+++ TipCalculate/main.js
@@ -6,23 +6,21 @@ let displayAmount = document.querySelector(".display-amount")
 let displayTotal = document.querySelector(".display-total")
 let notfelem = document.querySelector(".notf");
 let resetbtn = document.querySelector(".button button");
+let selectip=  document.querySelector(".row:nth-child(2) span");
 let percent = [];
-
-
 
 
 tips.forEach(item => {
     item.addEventListener("click", () => {
-        tips.forEach(item => item.classList.remove("active"))
-        item.classList.toggle("active")
-        resetbtn.className = "btnactive"
+        tips.forEach(item => item.classList.remove("active"));
+        item.classList.toggle("active");
+        resetbtn.className = "btnactive";
+        selectip.classList.remove("colred");
         percent.unshift(item.textContent.slice(0, -1));
         custom.value = ""
         displayAmount.textContent = `$${((billinput.value * percent[0]) / 100) / numpeopleinput.value}`
         displayTotal.textContent = `$${(+billinput.value + (billinput.value * percent[0]) / 100) / numpeopleinput.value}`
-       
     })
-
 
     resetbtn.addEventListener("click", () => {
         displayTotal.textContent = "$0.00"
@@ -42,8 +40,9 @@ tips.forEach(item => {
             displayTotal.textContent = "add bill!";
         }
         else {
-            item.classList.remove("active")
-            resetbtn.className = "btnactive"
+            item.classList.remove("active");
+            resetbtn.className = "btnactive";
+            selectip.classList.remove("colred");
             displayAmount.textContent = `$${((billinput.value * custom.value) / 100) / numpeopleinput.value}`
             displayTotal.textContent = `$${(+billinput.value + (billinput.value * custom.value) / 100) / numpeopleinput.value}`
         }
@@ -60,19 +59,27 @@ tips.forEach(item => {
 
     })
 
+    numpeopleinput.addEventListener("keyup", () => {
+        if (numpeopleinput.value == "" || numpeopleinput.value < 1) {
+            notfelem.innerHTML = "Can't be zero"
+            numpeopleinput.classList.add("numred")
+        }
+        else if (!percent[0]) {
+            notfelem.innerHTML = "";
+            numpeopleinput.classList.remove("numred");
+            selectip.classList.add("colred")
+            displayAmount.textContent = `$${billinput.value / numpeopleinput.value}`;
+            displayTotal.textContent = `$${billinput.value / numpeopleinput.value}`;
+            resetbtn.className = "btnactive"
+        }
+        else {
+            numpeopleinput.classList.remove("numred")
+            notfelem.innerHTML = ""
+            displayAmount.textContent = `$${((billinput.value * percent[0]) / 100) / numpeopleinput.value}`
+            displayTotal.textContent = `$${(+billinput.value + (billinput.value * percent[0]) / 100) / numpeopleinput.value}`
+        }
+        console.log(percent[0]);
+    })
 })
 
 
-numpeopleinput.addEventListener("keyup", () => {
-    if (numpeopleinput.value == "" || numpeopleinput.value < 1) {
-        notfelem.innerHTML = "Can't be zero"
-        numpeopleinput.classList.add("numred")
-    } else {
-        numpeopleinput.classList.remove("numred")
-        notfelem.innerHTML = ""
-        displayAmount.textContent = `$${((billinput.value * percent[0]) / 100) / numpeopleinput.value}`
-        displayTotal.textContent = `$${(+billinput.value + (billinput.value * percent[0]) / 100) / numpeopleinput.value}`
-    }
-
-
-})
