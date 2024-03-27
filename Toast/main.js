ab70eeb01fec6495ccda046299b1db911ef0ff2d
E1min <elmin708@gmail.com>
2024-03-27 20:18:27 +0400

    Toast Update

--- Toast/main.js
+++ Toast/main.js
@@ -4,45 +4,63 @@ let errorelem = document.getElementById("error")
 let sendbtn = document.querySelector("button")
 let display = document.querySelector(".display")
 let clearbtn = document.querySelector("button:last-child")
-let notf= document.querySelector("h5")
+let notf = document.querySelector("h5")
+let cancelelem = document.getElementById("iscancel")
+let nocancelelem = document.getElementById("nocancel")
+let durationelem = document.getElementById("duration")
 
 
 sendbtn.addEventListener("click", () => {
-    if(message.value.trim()===""){
-        notf.innerHTML="Please enter message!"
+
+    if (message.value.trim() === "") {
+        notf.innerHTML = "Please enter message!"
     }
-   else if (succeselem.checked) {
-        let divelem= document.createElement("div");
-        divelem.textContent=message.value
+    else if (succeselem.checked) {
+        let divelem = document.createElement("div");
+        divelem.textContent = message.value
         let deletebtn = document.createElement("button");
         deletebtn.textContent = "x"
-        deletebtn.style.backgroundColor="transparent"
-        deletebtn.style.border="none"
-        deletebtn.style.fontSize="16px"
+        deletebtn.style.backgroundColor = "transparent"
+        deletebtn.style.border = "none"
+        deletebtn.style.fontSize = "16px"
         display.appendChild(divelem)
-        divelem.appendChild(deletebtn)
-        divelem.className="succes"
-        deletebtn.addEventListener("click",()=>{
+        if (cancelelem.checked) {
+            divelem.appendChild(deletebtn)
+        }
+        else if (nocancelelem.checked) {
+            setTimeout(() => {
+                divelem.remove()
+            }, durationelem.value);
+        }
+        divelem.className = "succes"
+        deletebtn.addEventListener("click", () => {
             divelem.remove()
         })
-        notf.innerHTML=""
+        notf.innerHTML = ""
         message.value = ""
     }
     else if (errorelem.checked) {
-        let divelem= document.createElement("div");
-        divelem.textContent=message.value
+        let divelem = document.createElement("div");
+        divelem.textContent = message.value
         let deletebtn = document.createElement("button");
         deletebtn.textContent = "x"
-        deletebtn.style.backgroundColor="transparent"
-        deletebtn.style.border="none"
-        deletebtn.style.fontSize="16px"
-        display.appendChild(divelem)
-        divelem.appendChild(deletebtn)
-        divelem.className="error"
-        deletebtn.addEventListener("click",()=>{
+        deletebtn.style.backgroundColor = "transparent"
+        deletebtn.style.border = "none"
+        deletebtn.style.fontSize = "16px"
+        display.appendChild(divelem);
+        if (cancelelem.checked) {
+            divelem.appendChild(deletebtn)
+        }
+        else if (nocancelelem.checked) {
+            setTimeout(() => {
+                divelem.remove()
+            }, durationelem.value);
+        }
+        divelem.className = "error"
+        deletebtn.addEventListener("click", () => {
             divelem.remove()
         })
-        notf.innerHTML=""
+        notf.innerHTML = ""
         message.value = ""
 
     }
